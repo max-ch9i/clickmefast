45f84eff434b4204f7641c1edcbc64a35284a32c
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2015-12-21 10:13:07 +0200

    Add some styling

--- app/components/JoinControl.js
+++ app/components/JoinControl.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 export default class JoinControl extends React.Component {
+	_styles() {
+		return {
+			display: 'inline-block',
+			width: 100,
+			height: 100,
+			backgroundColor: 'magenta',
+			textAlign: 'center',
+			lineHeight: '100px',
+			color: 'white',
+			textDecoration: 'none'
+		};
+	}
 	controlClick(e) {
 		e.preventDefault();
 		this.props.click.call();
 	}
 	render() {
 		return (<div>
-			<a href onClick={this.controlClick.bind(this)}>Join</a>
+			<a style={this._styles()} href onClick={this.controlClick.bind(this)}>Join</a>
 		</div>);
 	}
 }
